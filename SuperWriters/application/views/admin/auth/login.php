17ca9015aa207896ddb362a2e19aa8ca88cc5336
Erra Dennis <39028505+Erraz@users.noreply.github.com>
2020-06-25 12:19:33 +0300

    Update login.php

--- SuperWriters/application/views/admin/auth/login.php
+++ SuperWriters/application/views/admin/auth/login.php
@@ -1,11 +1,14 @@
 
         <?php $this->load->view('admin/includes/_messages.php') ?>
- <div class="container">
-  <div class="frame">
+         <div class="container">
+          <div class="frame">
 
         <?php  echo form_open(base_url('admin/auth/login'), 'class="form-signin" '); ?>
 		
-		
+		<div class="form_header">
+            
+            
+        </div>   
   
           <label for="username">Username</label>
           <input class="form-styling" type="text" name="username" placeholder=""/>
@@ -14,15 +17,14 @@
           <input type="checkbox" id="checkbox"/>
           <label for="checkbox" ><span class="ui"></span>Keep me signed in</label>
 		  <p class="disclaimer">By Signing In you  agree to the terms presented in the <strong><span style ="color:#007bff">Terms and Conditions </span></strong> agreement.</p>
-<div class="erra">
+            <div class="erra">
           <div class="btn-animate">
 		  <input type="submit" class="btn-signin" name="submit" Value="Sign in">
-		  
           </div>
-		
+		    <p class="mb-1">
           <a href="<?= base_url('admin/auth/register'); ?>" class="text-center">Register</a>
-        </p>  
-		  
+            </p>  
+		 
 		  </div>
 	 <p class="mb-1">
           <a href="<?= base_url('admin/auth/forgot_password'); ?>">I forgot my password</a>
