f7c35414f8ead75881e08bb2ec85dfc0b1692202
Erra Dennis <39028505+Erraz@users.noreply.github.com>
2020-06-25 13:43:17 +0300

    Update login.php

--- SuperWriters/application/views/admin/auth/login.php
+++ SuperWriters/application/views/admin/auth/login.php
@@ -5,10 +5,12 @@
 
         <?php  echo form_open(base_url('admin/auth/login'), 'class="form-signin" '); ?>
 		
-		<div class="form_header">
-            
-            
-        </div>   
+		<div class="header">
+		<h1>SuperWriters</h1>
+    <h3><span>Sign in to your Account</span></h3>
+    
+   
+		</div>
   
           <label for="username">Username</label>
           <input class="form-styling" type="text" name="username" placeholder=""/>
@@ -17,15 +19,20 @@
           <input type="checkbox" id="checkbox"/>
           <label for="checkbox" ><span class="ui"></span>Keep me signed in</label>
 		  <p class="disclaimer">By Signing In you  agree to the terms presented in the <strong><span style ="color:#007bff">Terms and Conditions </span></strong> agreement.</p>
-            <div class="erra">
+      <div class="footer">
+<div class="erra">
+                
           <div class="btn-animate">
 		  <input type="submit" class="btn-signin" name="submit" Value="Sign in">
           </div>
+    
 		    <p class="mb-1">
-          <a href="<?= base_url('admin/auth/register'); ?>" class="text-center">Register</a>
+        <a href="<?= base_url('admin/auth/register'); ?>" class="text-center">Register</a>
             </p>  
-		 
-		  </div>
+ </div>
+   </div>           
+
+              
 	 <p class="mb-1">
           <a href="<?= base_url('admin/auth/forgot_password'); ?>">I forgot my password</a>
         </p>
